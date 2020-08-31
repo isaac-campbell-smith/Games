241f413ce4ab5b241bc93f36bd554b0c2f0eca79
isaac-campbell-smith <icampsmith@gmail.com>
2020-08-31 12:23:33 -0700

    Finished Connect 4 Score Checking System

--- Connect4/game.py
+++ Connect4/game.py
@@ -1,11 +1,19 @@
 from collections import defaultdict
 import numpy as np
 class Connect4():
+    """
+    Playable & Fully functioning skeleton for terminal-based Connect 4
+    TODO: 
+         1. Add starting message
+         2. Simulation option
+         3. Refactor diagonal checker for efficiency
+         4. Modularize checker 
+    """
 
     def __init__(self):
         self.col_check = defaultdict(int)
         self.player = 1
-        self.board = [defaultdict(list),defaultdict(list)]
+        self.board = np.full((6,7),'0')
         self.fours = ['1111','2222']
         self.game_over = None
         
@@ -13,25 +21,43 @@ class Connect4():
         self.player = 1 if self.player == 2 else 2
 
     def diagonal_check(self):
+        #CREATE ALL POSSIBLE WINNING DIAGONALS AS A COMPACTED ARR
+        #NEED TO REFACTOR TO NOT HAVE TO RECREATE EVERYTIME
         diagonals = []
-        diagonals.append([self.board[0][i][i] if self.board[0][i] else '' for i in range(6)])
-        diagonals.append([self.board[0][i][j] if self.board[0][i] else '' for i,j in zip(range(6), range(1,7))])
-        diagonals.append([self.board[0][i][j] if self.board[0][i] else '' for i,j in zip(range(1,6), range(5))])
-        diagonals.append([self.board[0][i][j] if self.board[0][i] else '' for i,j in zip(range(5), range(2,7))])
-        diagonals.append([self.board[0][i][j] if self.board[0][i] else '' for i,j in zip(range(2,6), range(4))])
-        diagonals.append([self.board[0][i][j] if self.board[0][i] else '' for i,j in zip(range(4), range(3,7))])
+        diagonals.append([self.board[i][i] for i in range(6)])
+        diagonals.append([self.board[i][j] for i,j in zip(range(6), range(1,7))])
+        diagonals.append([self.board[i][j] for i,j in zip(range(1,6), range(5))])
+        diagonals.append([self.board[i][j] for i,j in zip(range(5), range(2,7))])
+        diagonals.append([self.board[i][j] for i,j in zip(range(2,6), range(4))])
+        diagonals.append([self.board[i][j] for i,j in zip(range(4), range(3,7))])
+        
+        diagonals.append([self.board[-1-i][i] for i in range(6)])
+        diagonals.append([self.board[-1-i][j] for i,j in zip(range(6), range(1,7))])
+        diagonals.append([self.board[-1-i][j] for i,j in zip(range(1,6), range(5))])
+        diagonals.append([self.board[-1-i][j] for i,j in zip(range(5), range(2,7))])
+        diagonals.append([self.board[-1-i][j] for i,j in zip(range(2,6), range(4))])
+        diagonals.append([self.board[-1-i][j] for i,j in zip(range(4), range(3,7))])
         return diagonals
     
     def score_check(self,i):
-        for dic in self.board:
-            for arr in dic.values():
-                if self.fours[i] in ''.join(arr):
+        for row in self.board:
+            
+            if self.fours[i] in ''.join(row):
+                self.game_over='Game has finished!'
+                return f'Player {self.player} wins!'
+            
+        if max(self.col_check.values()) >= 4:
+            for col in self.board.T:
+                if self.fours[i] in ''.join(col):
+                    self.game_over='Game has finished!'
                     return f'Player {self.player} wins!'
-        if max(self.col_check) < 4:
-            diagonals = self.diagonal_check()
-            for arr in diagonals:
-                if self.fours[i] in ''.join(arr):
-                    return f'Player {self.player} wins!'
+                
+            if len(self.col_check) >= 4:
+                diagonals = self.diagonal_check()
+                for arr in diagonals:
+                    if self.fours[i] in ''.join(arr):
+                        self.game_over='Game has finished!'
+                        return f'Player {self.player} wins!'
 
         return f'Player {self.player} has a turn'
     
@@ -39,24 +65,20 @@ class Connect4():
         i = self.player - 1
         self.col_check[col] += 1
         
-        row = self.col_check[col]
-        if self.board[0]:
-            self.board[0][row][col] = str(i)
-        else:
-            self.board[0][row] = ['0' for _ in range(7)]
-            self.board[0][row][col] = str(i)
-        self.board[1][col].append(str(i))
-        #self.board[row, col] = str(self.player)
+        row = 6 - self.col_check[col]
+        
+        self.board[row, col] = str(self.player)
+
         msg = self.score_check(i)
         return msg
     
     def play(self, col):
-        if self.col_check:
-            print (max(self.col_check.values()))
-        if self.col_check[col] == 6:
-            return 'Column full!'
         if self.game_over:
             return self.game_over
+        
+        if self.col_check[col] == 6:
+            return 'Column full!'
+
         player = self.player
         msg = self.update_scores(col)
         self.change_player()
